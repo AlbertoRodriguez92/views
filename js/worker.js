bdcde45b7d15f086945f451c1566a0ae1a095661
Alberto Rodriguez <jslbrt.beto92@gmail.com>
2016-12-11 23:50:23 -0700

    Latest version dec/9/2016 3:00pm

---
+++ js/worker.js
@@ -0,0 +1,37 @@
+function timedCount() {
+
+self.addEventListener('message',function (e) {
+	var coor = e.data;
+	//Envío de datos al servidor
+	var valores = JSON.parse(coor);
+    var estado;
+
+var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+xmlhttp.open("POST", "http://localhost:8080/posiciones");
+xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xmlhttp.onprogress = function () {
+};
+    xmlhttp.onload = function () {
+        estado =  xmlhttp.readyState;
+        if(estado==4){
+            postMessage(estado);
+        }else{
+
+             }
+};
+xmlhttp.send(JSON.stringify({cliente: valores.cliente,
+        frecuencia:valores.frecuencia,
+        posicion:{lat:valores.posicion.lat,
+                  lon:valores.posicion.lon},
+        fechalocal: valores.fechalocal
+
+      }));
+
+},false);
+
+}
+
+timedCount();
+
+
